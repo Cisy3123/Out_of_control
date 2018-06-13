7c8610ebbe90c0c685adc255f7e8cdb3d6f981b5
CisyW <32593258+Cisy3123@users.noreply.github.com>
2018-06-14 01:25:14 +0800

    Add files via upload

---
+++ Classes/CardAction/CardAction.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "cocos2d.h"
+#include "CardSet.h"
+#include <vector>
+USING_NS_CC;
+class CardAction
+{
+public:
+	CardAction();
+	~CardAction();
+	bool shuffle();//œ¥≈∆
+	void deal();//∑¢≈∆
+	void sort();//’˚¿Ì≈∆
+
+};
